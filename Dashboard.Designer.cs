189084ba2420b7c40ab38d502186e81009f0ab71
Chancellor <Chancellor@DESKTOP-QG29NDM>
2026-03-12 23:59:54 -0400

    Add project files.

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace QuickVert
+{
+    partial class MainDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainDashboard));
+            this.sidebarPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.bntConverter = new System.Windows.Forms.Button();
+            this.btnHistory = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.lbDashboard = new System.Windows.Forms.Label();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sidebarPanel.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sidebarPanel
+            // 
+            this.sidebarPanel.BackColor = System.Drawing.Color.White;
+            this.sidebarPanel.ColumnCount = 4;
+            this.sidebarPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.sidebarPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.sidebarPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.sidebarPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.sidebarPanel.Controls.Add(this.btnSettings, 3, 0);
+            this.sidebarPanel.Controls.Add(this.btnHistory, 2, 0);
+            this.sidebarPanel.Controls.Add(this.bntConverter, 1, 0);
+            this.sidebarPanel.Controls.Add(this.btnDashboard, 0, 0);
+            this.sidebarPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.sidebarPanel.Location = new System.Drawing.Point(0, 0);
+            this.sidebarPanel.Name = "sidebarPanel";
+            this.sidebarPanel.RowCount = 1;
+            this.sidebarPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.sidebarPanel.Size = new System.Drawing.Size(1184, 100);
+            this.sidebarPanel.TabIndex = 1;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.Location = new System.Drawing.Point(0, 0);
+            this.btnDashboard.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(296, 100);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // bntConverter
+            // 
+            this.bntConverter.BackColor = System.Drawing.Color.AliceBlue;
+            this.bntConverter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bntConverter.FlatAppearance.BorderSize = 0;
+            this.bntConverter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bntConverter.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntConverter.Location = new System.Drawing.Point(296, 0);
+            this.bntConverter.Margin = new System.Windows.Forms.Padding(0);
+            this.bntConverter.Name = "bntConverter";
+            this.bntConverter.Size = new System.Drawing.Size(296, 100);
+            this.bntConverter.TabIndex = 1;
+            this.bntConverter.Text = "Converter";
+            this.bntConverter.UseVisualStyleBackColor = false;
+            // 
+            // btnHistory
+            // 
+            this.btnHistory.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnHistory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnHistory.FlatAppearance.BorderSize = 0;
+            this.btnHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHistory.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHistory.Location = new System.Drawing.Point(592, 0);
+            this.btnHistory.Margin = new System.Windows.Forms.Padding(0);
+            this.btnHistory.Name = "btnHistory";
+            this.btnHistory.Size = new System.Drawing.Size(296, 100);
+            this.btnHistory.TabIndex = 2;
+            this.btnHistory.Text = "History";
+            this.btnHistory.UseVisualStyleBackColor = false;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.Location = new System.Drawing.Point(888, 0);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(296, 100);
+            this.btnSettings.TabIndex = 3;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = false;
+            // 
+            // lbDashboard
+            // 
+            this.lbDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbDashboard.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDashboard.Location = new System.Drawing.Point(0, 0);
+            this.lbDashboard.Margin = new System.Windows.Forms.Padding(0);
+            this.lbDashboard.Name = "lbDashboard";
+            this.lbDashboard.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.lbDashboard.Size = new System.Drawing.Size(1184, 50);
+            this.lbDashboard.TabIndex = 0;
+            this.lbDashboard.Text = "Dashboard";
+            this.lbDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbDashboard.Click += new System.EventHandler(this.lbDashboard_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.GhostWhite;
+            this.mainPanel.Controls.Add(this.panel1);
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.lbDashboard);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 100);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1184, 611);
+            this.mainPanel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(0, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1184, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome back to QuickVert!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 540);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.label2.Size = new System.Drawing.Size(236, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total Conversions";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1184, 711);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.sidebarPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QuickVert";
+            this.sidebarPanel.ResumeLayout(false);
+            this.mainPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel sidebarPanel;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnHistory;
+        private System.Windows.Forms.Button bntConverter;
+        private System.Windows.Forms.Label lbDashboard;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Dashboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickVert
+{
+    public partial class MainDashboard : Form
+    {
+        public MainDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void lbDashboard_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
